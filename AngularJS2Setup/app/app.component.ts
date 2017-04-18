51d60caca7b1a4430294e8110df9091eb0115011
Gerry Whitworth <gerrywhitworth@sierrasystems.com>
2017-04-18 10:01:02 -0700

    Componet Bootstapping Done
    After Lesson 2

---
+++ AngularJS2Setup/app/app.component.ts
@@ -0,0 +1,9 @@
+﻿import {Component} from "@angular/core";
+
+@Component({
+    selector: "my-app",
+    template: "<h1>{{welcomeMessage}}</h1>"
+})
+export class AppComponent {
+    welcomeMessage: string = "Welcome to My 1st .NET Angular 2 project!!!";
+}
